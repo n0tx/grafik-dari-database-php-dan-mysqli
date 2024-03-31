4c6c4d372918230239da37f57fb6cce6bc7c3bdb
n0tx <rcandra91@msn.com>
2024-04-01 00:40:17 +0700

    update database connection

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,12 @@
 <?php 
-$koneksi = mysqli_connect("localhost:3306","root","","grafik_mahasiswa");
+// connect from termux mobile
+// $koneksi = mysqli_connect("localhost:3306","root","","grafik_mahasiswa");
+
+// connect from PC
+
+// juga bisa
+$koneksi = mysqli_connect("localhost:3306","root","root","grafik_mahasiswa");
+
+// bisa
+// $koneksi = mysqli_connect("localhost","root","root","grafik_mahasiswa");
 ?>
